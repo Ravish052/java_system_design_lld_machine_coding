83b2a2277d365f1b7a56e57b08dfb810cf73ee50
ravish463 <ravish.industill@gmail.com>
2026-01-07 13:39:48 +0530

    hash-map function done

--- hash-map/src/main/java/com/ravish/hashmap/Main.java
+++ hash-map/src/main/java/com/ravish/hashmap/Main.java
@@ -2,6 +2,14 @@ package com.ravish.hashmap;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hash map design code");
+        CustomHashMap<String, Integer> map = new CustomHashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+
+        System.out.println(map.get("a")); // 1
+        map.remove("a");
+        System.out.println(map.get("a")); // null
     }
 }
+
+
