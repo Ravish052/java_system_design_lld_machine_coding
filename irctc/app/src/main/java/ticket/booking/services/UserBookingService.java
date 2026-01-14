bd39cc36426ef4e15b0caeee5a9209cbba1acb2f
Ravish052 <bhatnagar1757@gmail.com>
2026-01-14 12:40:19 +0530

    added getters and setters for user class

--- irctc/app/src/main/java/ticket/booking/services/UserBookingService.java
+++ irctc/app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,4 +1,44 @@
 package ticket.booking.services;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
-public class UserBookingService {
+import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+
+public class UserBookingService
+{
+
+    private User user;
+
+    private List<User> userList;
+
+    private ObjectMapper objectMapper =  new ObjectMapper();
+
+    private static final  String USERS_PATH = "../localDb/users.json";
+
+
+
+    public UserBookingService(User user1) throws IOException
+    {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public boolean loginUser () {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+                return user1.getName().equals(user.getName()) &&
+                UserServiceUtil.checkPassword(user).findfirst();
+        return foundUser.isPresent();
+                )
+    }
+
+    public boolean signUp(User user1) {
+
+    }
 }
